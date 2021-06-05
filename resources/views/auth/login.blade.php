46dc815cd6355f851281ba49320763f70f715d37
chatomzcoding <coding.chatomz@gmail.com>
2021-06-05 19:32:27 +0700

    update halaman login dan redirect

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,3 @@
-{{-- <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Login') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout> --}}
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -70,7 +21,7 @@
     <div class="card-header">
       {{-- <a href="#" class="h1"><b>Chatomz</b>Company</a> --}}
       <section class="p-3 m-3">
-        <img src="{{ asset('/img/logo-bunefit.png')}}" alt="" class="img-fluid">
+        <img src="{{ asset('/img/cikara.png')}}" alt="" class="img-fluid">
       </section>
     </div>
     <div class="card-body">
@@ -100,21 +51,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
-            {{-- <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div> --}}
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">MASUK</button>
+          <div class="col">
+            <button type="submit" class="btn btn-outline-primary btn-block">MASUK</button>
             <br>
-          </div>
-          <div class="col-12">
-            <a href="{{ url('/')}}"><i class="fas fa-angle-left"></i> kembali ke Halaman Depan</a>
           </div>
           <!-- /.col -->
         </div>
